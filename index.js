c5810de25401daebdb23ec02279a80fbb030f4f6
wades1248 <wades1248@gmail.com>
2019-03-05 22:07:08 -0600

    fixed code formatting and mobile legned display

--- index.js
+++ index.js
@@ -1,147 +1,93 @@
 function handleStartButton(){
-
-$(".startPage").on("click", ".b1", function() {
-
-$(this).closest("div").detach();
-
-renderQuestion();
-
-showCounts();
-
-} )
-
+	$(".startPage").on("click", ".b1", function() {
+		$(this).closest("div").detach();
+		renderQuestion();
+		showCounts();
+		} 
+	)
 }
 
 function handleSubmit(){
-
-$('.jsQuestions').submit(event => {
-
-event.preventDefault();
-
-let selected = $('input:checked').val();
-
-if (selected === Answers[questionNum]){
-
-renderAnswerCorrect();
-
-incrementScore();
-
-} else {
-
-renderAnswerIncorrect();
-
+	$('.jsQuestions').submit(event => {
+		event.preventDefault();
+		let selected = $('input:checked').val();
+		if (selected === Answers[questionNum]){
+			renderAnswerCorrect();
+			incrementScore();
+		} else {
+			renderAnswerIncorrect();
+		}
+		renderCounts();
+	}
+	)
 }
-
-renderCounts();
-
-}
-
-)
-
-
-}
-
 
 function handleNextQ(){
-
-$('.jsQuestions').on("click", ".b3", function(){
-
-renderCounts();
-
-if (questionNum === 9) {
-
-createResults();
-
-showCounts();
-
-} else {
-
-incrementQ();
-
-renderCounts();
-
-renderQuestion();
-
-}
-
-})
-
+	$('.jsQuestions').on("click", ".b3", function(){
+		renderCounts();
+		if (questionNum === 9) {
+			createResults();
+			showCounts();
+		} else {
+			incrementQ();
+			renderCounts();
+			renderQuestion();
+		}
+	}
+	)
 }
 
 function handleRestart(){
-
-$('.jsQuestions').on("click", ".b4", function(event){
-
-document.location.reload();
-
-})
-
+	$('.jsQuestions').on("click", ".b4", function(event){
+	document.location.reload();
+	}
+	)
 }
 
 function incrementQ(){
-
-questionNum ++;
-
+	questionNum ++;
 }
-
 
 function incrementScore(){
-
-score ++;
-
+	score ++;
 }
 
-
-function showCounts(){$('ul.scores').toggleClass("hidden");}
+function showCounts(){
+	$('ul.scores').toggleClass("hidden");
+}
 
 
 function createResults(){
-
-$('.jsQuestions').html(resultGen(score)); 
-
+	$('.jsQuestions').html(resultGen(score)); 
 }
 
-
 function renderCounts(){
-
-$(".qNo").text(`Question: ${questionNum+1}/10`);
-
-$(".sNo").text(`Correct Answers: ${score}`);
-
+	$(".qNumber").text(`Question: ${questionNum+1}/10`);
+	$(".sNumber").text(`Correct Answers: ${score}`);
 }
 
 
 function renderAnswerCorrect(){
-
-$('.jsQuestions').html(CorrectGen(questionNum));
-
-};
+	$('.jsQuestions').html(CorrectGen(questionNum));
+}
 
 
 function renderAnswerIncorrect(){
-
-$('.jsQuestions').html(IncorrectGen(questionNum))};
+	$('.jsQuestions').html(IncorrectGen(questionNum))
+}
 
 
 function renderQuestion(){
-
-$('.jsQuestions').html(Qgen(questionNum));
-
+	$('.jsQuestions').html(Qgen(questionNum));
 }
 
 
 function initialize(){
-
-handleStartButton();
-
-handleSubmit();
-
-handleNextQ();
-
-renderCounts();
-
-handleRestart();
-
+	handleStartButton();
+	handleSubmit();
+	handleNextQ();
+	renderCounts();
+	handleRestart();
 }
 
 $(initialize);
